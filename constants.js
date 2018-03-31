1ae37a823b6ff0cd1d714a2e2f7a485d03a21b41
liuxing <Clarelau61803@gmail.com>
2018-03-31 11:50:14 +0800

    update constants.js

--- constants.js
+++ constants.js
@@ -7,8 +7,8 @@ exports.TOTAL_WHITEBYTES = 1e15;
 exports.MAJORITY_OF_WITNESSES = (exports.COUNT_WITNESSES%2===0) ? (exports.COUNT_WITNESSES/2+1) : Math.ceil(exports.COUNT_WITNESSES/2);
 exports.COUNT_MC_BALLS_FOR_PAID_WITNESSING = 100;
 
-exports.version = '1.0t';
-exports.alt = '2';
+exports.version = '1.0';
+exports.alt = '1';
 
 exports.GENESIS_UNIT = (exports.alt === '2' && exports.version === '1.0t') ? 'TvqutGPz3T4Cs6oiChxFlclY92M2MvCvfXR5/FETato=' : 'oj8yEksX9Ubq7lLc+p6F2uyHUuynugeVq4+ikT67X6E=';
 exports.BLACKBYTES_ASSET = (exports.alt === '2' && exports.version === '1.0t') ? 'LUQu5ik4WLfCrr8OwXezqBa+i3IlZLqxj2itQZQm8WY=' : 'qO2JsiuDMh/j+pqJYZw3u82O71WjCDf0vTNvsnntr8o=';
@@ -32,6 +32,3 @@ exports.MAX_DATA_FEED_VALUE_LENGTH = 64;
 exports.MAX_AUTHENTIFIER_LENGTH = 4096;
 exports.MAX_CAP = 9e15;
 exports.MAX_COMPLEXITY = 100;
-
-exports.TEXTCOIN_CLAIM_FEE = 548;
-exports.TEXTCOIN_ASSET_CLAIM_FEE = 750;
